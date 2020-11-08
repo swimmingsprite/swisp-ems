23c85ee51de7e2a5750e9523c7ab202ff487bed9
swimmingsprite <f.chomjak@gmail.com>
2020-11-08 14:45:58 +0100

    shift panel

--- src/components/main/panel/ShiftPanel.jsx
+++ src/components/main/panel/ShiftPanel.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import {Avatar} from "@material-ui/core";
 import {AvatarGroup} from '@material-ui/lab';
-import { makeStyles } from '@material-ui/core/styles';
+import {makeStyles} from '@material-ui/core/styles';
 import createStyles from "@material-ui/styles/createStyles";
-
+import deepOrange from "@material-ui/core/colors/deepOrange";
+import deepPurple from "@material-ui/core/colors/deepPurple";
 
 
 /* var shifts = [
@@ -21,6 +22,9 @@ import createStyles from "@material-ui/styles/createStyles";
 
 function ShiftElement(props) {
     /*do propsu cely objekt jednej smeny*/
+    var aColor = "red";
+
+    console.log("acolor: " + aColor)
 
     const styles = makeStyles((theme) => createStyles({
         avatar: {
@@ -32,20 +36,62 @@ function ShiftElement(props) {
 
     const classes = styles();
 
+    /*todo MAP AVATARS FUNCTION*/
+
+    const avStyles = makeStyles((theme) => createStyles({
+        orange: {
+            color: theme.palette.getContrastText(deepOrange[500]),
+            backgroundColor: "red",
+            backgroundSize: "cover",
+            backgroundPosition: "top center"
+
+        },
+        purple: {
+            color: theme.palette.getContrastText(deepPurple[500]),
+            backgroundColor: deepPurple[500],
+        }
+    }))
+
+    var avClasses = avStyles();
 
     return <div className="shift-element">
+        <div style={{display: "inline-block"}}>
 
-        <AvatarGroup max={3}
-                     classes={{avatar: classes.avatar}}
+            <AvatarGroup max={3}
+                         classes={{avatar: classes.avatar}}
+                         style={{marginLeft: "5px", display: "flex"}}>
+                {/*<Avatar alt="Remy Sharp"
+                    className={avClasses.orange}
+                    style={{backgroundImage: "url(https://i.insider.com/5484d9d1eab8ea3017b17e29?width=800&format=jpeg&auto=webp)"}}
+            />
+            <Avatar alt="Vemy Sharp" src="/static/images/avatar/1.jpg" style={{backgroundColor: "blue"}}/>
+            <Avatar alt="Gemy Sharp" src="/static/images/avatar/1.jpg" style={{backgroundColor: "green"}}/>
+            <Avatar alt="Aemy Sharp" src="/static/images/avatar/1.jpg" style={{backgroundColor: "purple"}}/>
+            <Avatar alt="Hemy Sharp" src="/static/images/avatar/1.jpg"/>*/}
+                {/*<Avatar src='htctps://www.fillmurray.com/500/900' size={100} backgroundColor='rgba(199,99,128,0.8)' />*/}
+                <Avatar
+                    /*src="https://www.fillmurray.com/500/900"*/
+                    className={avClasses.orange}
+                />
+                <Avatar
+                    /*src="https://www.fillmurray.com/500/900"*/
+                    className={avClasses.orange}
+                />
+                <Avatar
+                    /*src="https://www.fillmurray.com/500/900"*/
+                    className={avClasses.orange}
+                />
+                <Avatar
+                    /*src="https://www.fillmurray.com/500/900"*/
+                    className={avClasses.orange}
+                />
 
-        >
-            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg"/>
-            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg"/>
-            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg"/>
-            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg"/>
-            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg"/>
+            </AvatarGroup>
 
-        </AvatarGroup>
+        </div>
+
+
+        <h1 className={"shift-element-header"}>John Barney, Silvia Cloak, Amanda Header</h1>
     </div>
 }
 
@@ -65,25 +111,25 @@ export default function ShiftPanel(props) {
 
 
     return <div className="post-status-bar"
-                style={{width: "100%"}}>
+                style={{width: "100%", minWidth: "440px"}}>
         <div className="shift-content">
-            <table className="shift-content-table">
-                <tr>
-                    <td className="shift-content-td">8:00</td>
-                    <td className="shift-content-td">9:00</td>
-                    <td className="shift-content-td">10:00</td>
-                    <td className="shift-content-td">11:00</td>
-                    <td className="shift-content-td">12:00</td>
-                    <td className="shift-content-td">13:00</td>
-                    <td className="shift-content-td">14:00</td>
-                    <td className="shift-content-td">15:00</td>
-                    <td className="shift-content-td">16:00</td>
-                    <td className="shift-content-td">17:00</td>
-                </tr>
-                <tr>
+            <div className="shift-content-table">
+                <div>
+                    <div className="shift-content-div">8:00</div>
+                    <div className="shift-content-div">9:00</div>
+                    <div className="shift-content-div">10:00</div>
+                    <div className="shift-content-div">11:00</div>
+                    <div className="shift-content-div">12:00</div>
+                    <div className="shift-content-div">13:00</div>
+                    <div className="shift-content-div">14:00</div>
+                    <div className="shift-content-div">15:00</div>
+                    <div className="shift-content-div">16:00</div>
+                    <div className="shift-content-div">17:00</div>
+                </div>
+
                 <ShiftElement value={shifts[0]}/>
-                </tr>
-            </table>
+
+            </div>
         </div>
 
 
