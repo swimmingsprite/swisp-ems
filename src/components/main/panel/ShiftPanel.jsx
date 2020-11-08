4820ed106db91524cd756fcee52e0241c78e69b4
swimmingsprite <f.chomjak@gmail.com>
2020-11-08 16:53:11 +0100

    shift content filter

--- src/components/main/panel/ShiftPanel.jsx
+++ src/components/main/panel/ShiftPanel.jsx
@@ -59,16 +59,8 @@ function ShiftElement(props) {
 
             <AvatarGroup max={3}
                          classes={{avatar: classes.avatar}}
-                         style={{marginLeft: "5px", display: "flex"}}>
-                {/*<Avatar alt="Remy Sharp"
-                    className={avClasses.orange}
-                    style={{backgroundImage: "url(https://i.insider.com/5484d9d1eab8ea3017b17e29?width=800&format=jpeg&auto=webp)"}}
-            />
-            <Avatar alt="Vemy Sharp" src="/static/images/avatar/1.jpg" style={{backgroundColor: "blue"}}/>
-            <Avatar alt="Gemy Sharp" src="/static/images/avatar/1.jpg" style={{backgroundColor: "green"}}/>
-            <Avatar alt="Aemy Sharp" src="/static/images/avatar/1.jpg" style={{backgroundColor: "purple"}}/>
-            <Avatar alt="Hemy Sharp" src="/static/images/avatar/1.jpg"/>*/}
-                {/*<Avatar src='htctps://www.fillmurray.com/500/900' size={100} backgroundColor='rgba(199,99,128,0.8)' />*/}
+                         style={{marginLeft: "5px"}}>
+
                 <Avatar
                     /*src="https://www.fillmurray.com/500/900"*/
                     className={avClasses.orange}
@@ -95,6 +87,7 @@ function ShiftElement(props) {
     </div>
 }
 
+
 export default function ShiftPanel(props) {
     var shifts = [
         {
@@ -109,25 +102,71 @@ export default function ShiftPanel(props) {
         },
     ]
 
+    function mapHoursToDivs(cView) {
+        var hours = [];
+        for (var x = cView.startTimestamp; x < cView.endTimestamp; ) {
+            hours.push(new Date(x).getHours());
+            x += 3600000;
+        }
+
+        var divs = [];
+        hours.forEach(value => {
+            divs.push(
+            <div className="shift-content-div">{value}:00</div>
+            );
+        })
+
+        return divs;
+
+    }
+
+    var time = new Date().setHours(new Date().getHours(),0,0,0);
+    var startTimestamp = new Date(time).getTime()-18000000
+
+    var time2 = new Date().setHours(new Date().getHours(),0,0,0);
+    var endTimestamp = new Date(time).getTime()+18000000-1
+
+    var test = new Date(startTimestamp).toLocaleTimeString();
+    var test2 = new Date(endTimestamp).toLocaleTimeString();
+    console.log("TEST: "+test);
+    console.log("TEST 2: "+test2);
+
+    var [currentView, setCurrentView] = React.useState({
+        startHour: new Date().getHours() - 5,
+        endHour: new Date().getHours() + 4,
+        currentTimestamp: new Date().getTime(),
+        startTimestamp: startTimestamp,
+        endTimestamp: endTimestamp,
+    })
+
+    console.log("start: "+currentView.start+" " +
+        "end: "+currentView.end+" " +
+        "current: "+currentView.current+
+        "start timestamp:" +currentView.startTimestamp);
+
 
     return <div className="post-status-bar"
                 style={{width: "100%", minWidth: "440px"}}>
         <div className="shift-content">
             <div className="shift-content-table">
                 <div>
-                    <div className="shift-content-div">8:00</div>
-                    <div className="shift-content-div">9:00</div>
-                    <div className="shift-content-div">10:00</div>
-                    <div className="shift-content-div">11:00</div>
-                    <div className="shift-content-div">12:00</div>
-                    <div className="shift-content-div">13:00</div>
-                    <div className="shift-content-div">14:00</div>
-                    <div className="shift-content-div">15:00</div>
-                    <div className="shift-content-div">16:00</div>
-                    <div className="shift-content-div">17:00</div>
+                    {mapHoursToDivs(currentView)}
                 </div>
 
-                <ShiftElement value={shifts[0]}/>
+                {/*MAP SHIFTS TO SHIFT ELEMENTS*/}
+
+                {shifts
+                    .filter(value => {
+                        if (value.start >= currentView.startTimestamp && value.start < currentView.endTimestamp) return true;
+                        if (value.end >= currentView.startTimestamp && value.end < currentView.endTimestamp) return true;
+                        if (value.start < currentView.startTimestamp && value.end > currentView.endTimestamp) return true;
+                        return false;
+                    })
+                    .map(value => {
+                    return <ShiftElement value={value}/>
+                })}
+
+
 
             </div>
         </div>
