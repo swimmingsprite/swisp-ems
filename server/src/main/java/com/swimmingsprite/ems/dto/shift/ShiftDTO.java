e4f7406563a56e2e95c67a9ba315f521de253d69
swimmingsprite <f.chomjak@gmail.com>
2021-01-20 16:43:55 +0100

    shift dto and repository

---
+++ server/src/main/java/com/swimmingsprite/ems/dto/shift/ShiftDTO.java
@@ -0,0 +1,81 @@
+package com.swimmingsprite.ems.dto.shift;
+
+import com.swimmingsprite.ems.model.Department;
+import com.swimmingsprite.ems.model.user.AvatarUserImpl;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.Instant;
+import java.util.Set;
+
+public class ShiftDTO {
+    private String id;
+    private Set<AvatarUserImpl> employees;
+    private String placeId;
+    private String departmentId;
+    private Instant start;
+    private Instant end;
+
+    public ShiftDTO(String id,
+                    Set<AvatarUserImpl> employees,
+                    String placeId,
+                    String departmentId,
+                    Instant startTime,
+                    Instant endTime) {
+        this.id = id;
+        this.employees = employees;
+        this.placeId = placeId;
+        this.departmentId = departmentId;
+        this.start = startTime;
+        this.end = endTime;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Set<AvatarUserImpl> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(Set<AvatarUserImpl> employees) {
+        this.employees = employees;
+    }
+
+    public String getPlaceId() {
+        return placeId;
+    }
+
+    public void setPlaceId(String placeId) {
+        this.placeId = placeId;
+    }
+
+    public String getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(String departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public Instant getStart() {
+        return start;
+    }
+
+    public void setStart(Instant start) {
+        this.start = start;
+    }
+
+    public Instant getEnd() {
+        return end;
+    }
+
+    public void setEnd(Instant end) {
+        this.end = end;
+    }
+}
---
+++ server/src/main/java/com/swimmingsprite/ems/repository/shift/ShiftRepository.java
@@ -0,0 +1,22 @@
+package com.swimmingsprite.ems.repository.shift;
+
+import com.swimmingsprite.ems.dto.shift.ShiftDTO;
+import com.swimmingsprite.ems.model.Shift;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ShiftRepository extends JpaRepository<Shift, String> {
+
+    @Query("select new com.swimmingsprite.ems.dto.shift.ShiftDTO(" +
+            "s.id , s.shiftEmployees, s.department.place.id, s.department.id, s.startTime, s.endTime ) " +
+            "from Shift s " +
+            "where s.department.id = ?1 " +
+            "and (s.startTime >= ?2 and s.startTime <= ?3) " +
+            "or (s.endTime >= ?2 and s.endTime <= ?3) " +
+            "order by s.startTime asc")
+    List<ShiftDTO> getAllShiftsByTimeRangeAndDepartment(String departmentId, long startTimestamp, long endTimestamp);
+}
